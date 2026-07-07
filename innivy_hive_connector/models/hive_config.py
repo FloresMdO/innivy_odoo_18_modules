1898fdda5a80692ad951b492919b7616a9024d0a
FloresMdO <ivaanfmdo@gmail.com>
2026-07-07 12:42:44 -0600

    Agrega logica y vistas de configuracion

---
+++ innivy_hive_connector/models/hive_config.py
@@ -0,0 +1,35 @@
+
+# -*- coding: utf-8 -*-
+from dataclasses import field
+from odoo import models, fields, api
+
+class HiveConfig(models.TransientModel):
+    _name = 'hive.config'
+    _description = "Hive Configuration"
+    
+    name = fields.Char(
+        required=True
+    )
+    
+    hive_url = fields.Char(
+        string="HIVE API URL", 
+        default='http://localhost:18790'
+    )
+    hive_token = fields.Char(
+        string="HIVE AUTH TOKEN",
+    )
+    is_hive_enabled = fields.Boolean(
+        string="ENABLED",
+        compute='_compute_is_hive_enabled',
+        readonly=False
+    )
+    
+    def _compute_is_hive_enabled(self):
+        for record in self:
+            if record.hive_url and record.hive_token:
+                record.is_hive_enabled = True 
+            else:
+                record.is_hive_enabled = False 
+        
+    
+    
