23a309fad54476c7ddf099afbeabc1d649929447
FloresMdO <ivaanfmdo@gmail.com>
2026-07-06 20:28:52 -0600

    Agrega modulo 'innivy_hive_connector'

---
+++ innivy_hive_connector/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Innivy Hive Connector",
+    "version": "18.0.0.1",
+    "author": "Innivy",
+    "category": "Connector",
+    "summary": "Connect Odoo to Hive",
+    "description": "Connect Odoo to Hive",
+    "depends": ["base"],
+    "data": [],
+    "installable": True,
+    "application": True,
+    "auto_install": False,
+}
